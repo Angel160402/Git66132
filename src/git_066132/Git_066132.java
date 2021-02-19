a128643ace99b50665e713e0440b8d1e8930ad87
User <User@DESKTOP-AMQNG48>
2021-02-18 20:29:49 -0600

    Mi primer commit

---
+++ src/git_066132/Git_066132.java
@@ -0,0 +1,25 @@
+
+package git_066132;
+
+
+public class Git_066132 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    String nombre = "Angel Gabriel";
+    String matricula = "66132";
+    
+    System.out.println("Soy = " + nombre + "Y mi matricula es " + matricula);
+    
+      int edad =0;
+    
+    
+    }
+    
+    
+    
+    
+   }
